e0236b624f935467952f5c936ddab514e99cbdc6
Jamie Lopez <jmelopez@outlook.com>
2020-09-09 20:28:27 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,13 @@
+function updateObjectWithKeyAndValue(object,key,value) {
+  return Object.assign ({}, object, {[key]: value})
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object,key,value) {
+  object[key] = value
+  return object
+}
+
+function deleteFromObjectByKey(object, key) {
+  Object.assign({}, object)
+  delete object[key]
+  }
